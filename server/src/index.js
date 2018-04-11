8a15b7484cb2feb3ed6a63ed709a669adf654bf0
Elisabeth Seite <elisabeth.seite@gmail.com>
2018-04-11 18:13:14 -0400

    Implemented basic front end

--- server/src/index.js
+++ server/src/index.js
@@ -19,7 +19,7 @@ const server = new GraphQLServer({
     ...req,
     db: new Prisma({
       typeDefs: 'src/generated/prisma.graphql',
-      endpoint: '__PRISMA_ENDPOINT__',
+      endpoint: 'https://eu1.prisma.sh/public-malachiterobin-390/hackernews-graphql-js/dev',
       secret: 'mysecret123',
       debug: true
     }),
